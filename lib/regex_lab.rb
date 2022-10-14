eb3bd4d580751f433e84c12a2d9ad6780ad9d96c
Parker Parham <parkerparham@gmail.com>
2022-10-14 15:31:01 -0400

    Done with assignment

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,31 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    if word.match(/\A[aeuioAEUIO]/)
+        return true
+   else
+    return false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w*ing\b/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+#     if text.match(/\b[A-Z].*[.!?"'`]/)
+#         return true
+#    else
+#     return false
+#    end
+    text.match(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
 end
 
-def valid_phone_number?(phone)
 
+
+def valid_phone_number?(phone)
+    phone.match(/^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/) ? true : false
 end
